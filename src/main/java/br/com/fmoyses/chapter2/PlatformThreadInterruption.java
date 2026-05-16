63a3f77356b94a3a09125c7e08036a5d053a34b9
felipejsm <felipejsm41@gmail.com.br>
2026-05-16 19:43:28 -0300

    chore: using executors

--- src/main/java/br/com/fmoyses/chapter2/PlatformThreadInterruption.java
+++ src/main/java/br/com/fmoyses/chapter2/PlatformThreadInterruption.java
@@ -1,8 +1,14 @@
 package br.com.fmoyses.chapter2;
 
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 public class PlatformThreadInterruption {
-    void main() {
-        virtualMe();
+    void main() throws InterruptedException {
+        vtDemo();
     }
     static void platformMe() {
         Thread platformThread = Thread.ofPlatform().start(() -> {
@@ -44,6 +50,35 @@ public class PlatformThreadInterruption {
         } catch (InterruptedException e) {
             // TODO: handle exception
             virtualThread.interrupt();
+        }
+    }
+    static void threadGroup() throws InterruptedException {
+        Set<ThreadGroup> threadGroups = new HashSet<>();
+        for (int i = 0; i < 100; i++) {
+            Thread vThread = Thread.ofVirtual().start(() -> {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                    Thread.currentThread().interrupt();
+                }
+            });
+            threadGroups.add(vThread.getThreadGroup());
+            Thread.sleep(1000);
+            IO.println("Unique thread groups: " + threadGroups.size());
+            IO.println("Thread group " + threadGroups.iterator().next());
+        }
+    }
+
+    static void vtDemo() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+        //try (var executor = Executors.newCachedThreadPool()) {
+            IntStream.range(0, 10_000).forEach(i -> {
+                executor.submit(() -> {
+                    Thread.sleep(Duration.ofSeconds(1));
+                    return i;
+                });
+            });
         }
     }
 }
